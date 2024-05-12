d620ff584a5aa3fbd5db450abc85bb590ef2bc69
Frodonik <niki.shara@mail.ru>
2024-05-12 14:27:32 +0300

    Update games.js

--- routers/games.js
+++ routers/games.js
@@ -6,7 +6,6 @@ const {sendAllGames, sendGameCreated, sendGameById, sendGameUpdated, sendGameDel
 gamesRouter.get('/games', findAllGames, sendAllGames);
 gamesRouter.post("/games", findAllGames, checkIsGameExists, checkIfCategoriesAvaliable, checkEmptyFields, createGame, sendGameCreated);
 gamesRouter.get("/games/:id", findGameById, sendGameById);
-gamesRouter.put("/games/:id", findGameById, checkIfUsersAreSafe, checkIfCategoriesAvaliable, checkEmptyFields updateGame, checkEmptyFields, sendGameUpdated)
 gamesRouter.put("/games/:id", findGameById, checkIfUsersAreSafe, checkIfCategoriesAvaliable, checkEmptyFields, updateGame, checkEmptyFields, sendGameUpdated)
 gamesRouter.delete("/games/:id", deleteGame, sendGameDeleted)
 
