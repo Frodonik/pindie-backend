e506993f535efde21a462e63b0d4537255a9aece
Frodonik <niki.shara@mail.ru>
2024-05-21 10:04:26 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,14 +1,20 @@
-const allowedCors = ['https://blackgate.nomoredomainswork.ru/']; 
+const allowedCors = [
+    'https://blackgate.nomoredomainswork.ru',
+    'http://localhost:3000',
+    'http://localhost:3001'
+]; 
 
 function cors(req, res, next) {
     const { origin } = req.headers;
-  
-    if (allowedCors.includes(origin)) {
-      res.header("Access-Control-Allow-Origin", origin);
+    
+    if (allowedCors.includes(origin)) { 
+        res.header('Access-Control-Allow-Origin', origin);
     }
     res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next();
-  }
-  
-  module.exports = cors; 
+}
+
+module.exports = {
+    cors
+}
